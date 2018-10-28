71f3fc6b54899345ab14b729ca2131c8c15ee8e1
sourabhmishra <mishra.sourabh80@gmail.com>
2018-10-28 16:37:44 +0530

    Assigment12 Question1

---
+++ CalculatorExecutor.js
@@ -0,0 +1,24 @@
+var calculator = require('./calculator.js');
+var rs = require('readline-sync');
+
+//Calculator
+var x = rs.question("Please enter a number ");
+var symbol = rs.question('Please enter an operator (+, - , / , * ) ');
+var y = rs.question('Please enter another number ');
+
+switch(symbol){
+case '+':
+calculator.add(x, y);
+break;
+case '-':
+calculator.subtract(x, y);
+break;
+case '*':
+calculator.multiply(x,y);
+break;
+case '/':
+calculator.divide(x, y);
+break;
+default:
+console.log("Invalid Entry was made!!");
+}
---
+++ CalculatorModule.js
@@ -0,0 +1,20 @@
+var calculator = {
+
+    add: function(x,y){
+    console.log(x + y);
+    },
+    
+    subtract: function(x,y){
+    console.log(x - y);
+    },
+    
+    multiply: function(x,y){
+    console.log(x * y);
+    },
+    
+    divide: function(x,y){
+    console.log(x / y);
+    },
+    }
+    module.exports = calculator;
+    
